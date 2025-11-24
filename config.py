b85b0ecddef405123cb91d376caf44cc3f52772b
rcbcc25-sketch <rcbcc25@gmail.com>
2025-11-24 18:30:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Admin configuration
+ADMIN_IDS = [8050673236]  # Add your admin IDs here
+
+# MongoDB configuration
+MONGODB_URI = "mongodb+srv://CHOCHAN00:<CHOCHAN00>@cluster0.kcx9pkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
---
+++ vars.py
@@ -0,0 +1,12 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+from os import environ
+
+API_ID = int(environ.get("API_ID", "29731917"))
+API_HASH = environ.get("API_HASH", "d0b73a75f2d12cae5b04c21044ff0148")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8074079816:AAEOGZFCBSl9OkqwaOS-m8xj6X9uzYfQVQk")
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
+
