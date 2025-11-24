14a6b6e03d40b989c22ad6b484ae69f213186d07
rcbcc25-sketch <rcbcc25@gmail.com>
2025-11-25 01:31:49 +0530

    Update BOT_TOKEN in vars.py
    Updated BOT_TOKEN to a new value while retaining API_ID and API_HASH.

--- vars.py
+++ vars.py
@@ -4,9 +4,10 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29731917"))
 API_HASH = environ.get("API_HASH", "d0b73a75f2d12cae5b04c21044ff0148")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8074079816:AAEOGZFCBSl9OkqwaOS-m8xj6X9uzYfQVQk")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8179440986:AAFSNAqCPnYe1M9DBMyn4hr4jSd40zpYK1s")
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
